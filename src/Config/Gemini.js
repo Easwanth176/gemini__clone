109e73fdcdbdacdb84d8bc15fed5b99d5a97b07d
Easwanth konduru <103351652+Easwanth176@users.noreply.github.com>
2024-06-07 23:15:49 +0530

    Update Gemini.js

--- src/Config/Gemini.js
+++ src/Config/Gemini.js
@@ -5,7 +5,7 @@ const {
   } = require("@google/generative-ai");
   
   //const apiKey = process.env.GEMINI_API_KEY;
-  const apiKey ="AIzaSyDgtQhe2mm_KxSjKrI5A0zsrJ9LGS_NeN8";
+  const apiKey ="";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
